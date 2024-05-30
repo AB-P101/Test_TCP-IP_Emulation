3e196b9c0b37c7458631ea57c14ea83eb374fb26
AB101 <133328690+Atsadaphong@users.noreply.github.com>
2024-05-30 10:28:36 +0700

    Add files via upload

---
+++ program/Code/PGFLEXSIM/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txt_host = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_port = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_add = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(12, 310)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(158, 52)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "Send"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(140, 252)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(408, 41)
+        Me.TextBox1.TabIndex = 1
+        '
+        'txt_host
+        '
+        Me.txt_host.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_host.Location = New System.Drawing.Point(18, 48)
+        Me.txt_host.Name = "txt_host"
+        Me.txt_host.Size = New System.Drawing.Size(379, 41)
+        Me.txt_host.TabIndex = 2
+        Me.txt_host.Text = "127.0.0.6"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 31)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Host:"
+        '
+        'txt_port
+        '
+        Me.txt_port.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_port.Location = New System.Drawing.Point(403, 48)
+        Me.txt_port.Name = "txt_port"
+        Me.txt_port.Size = New System.Drawing.Size(145, 41)
+        Me.txt_port.TabIndex = 4
+        Me.txt_port.Text = "502"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(397, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 31)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Port:"
+        '
+        'txt_add
+        '
+        Me.txt_add.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_add.Location = New System.Drawing.Point(12, 252)
+        Me.txt_add.Name = "txt_add"
+        Me.txt_add.Size = New System.Drawing.Size(122, 41)
+        Me.txt_add.TabIndex = 6
+        Me.txt_add.Text = "101"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 218)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(122, 31)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Address:"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(18, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(158, 52)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(272, 119)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 25)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Wait"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(187, 119)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 25)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Status :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(134, 218)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(80, 31)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Data:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(176, 327)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 25)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Status :"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(272, 327)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(52, 25)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Wait"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(560, 388)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txt_add)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_port)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_host)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Test_Send_To_FlexSIM"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt_host As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_port As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txt_add As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ program/Code/PGFLEXSIM/WindowsApplication1/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+
+Public Class Form1
+
+    Private mSocket As Socket = Nothing
+    'Private IP As String = txt_host.Text
+    'Private Port As UShort = txt_port.Text
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Connect()
+
+
+        Me.mSocket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        Me.mSocket.ReceiveBufferSize = 256
+        Me.mSocket.SendBufferSize = 256
+        Dim server As IPEndPoint = New IPEndPoint(IPAddress.Parse(txt_host.Text), txt_port.Text)
+        Me.mSocket.Connect(server)
+    End Sub
+
+    Private Sub Disconnect()
+        If Not IsDBNull(Me.mSocket) Then
+            Me.mSocket.Close()
+        End If
+    End Sub
+
+
+    Private Function WriteSingleCoil(ByVal id As UShort, ByVal slaveAddress As Byte, ByVal startAddress As UShort, ByVal functionCode As Byte, ByVal data As UShort) As Byte()
+        Dim frame As Byte() = New Byte(11) {} ' Total 12 Bytes.
+
+        'Convert ushort to byte aray
+        Dim byteArray As Byte() = BitConverter.GetBytes(data)
+
+        frame(0) = CByte(id / 256) ' Transaction Identifier High. 
+        frame(1) = CByte(id Mod 256) ' Transaction Identifier Low.
+        frame(2) = 0 ' Protocol Identifier High.
+        frame(3) = 0 ' Protocol Identifier Low.
+        frame(4) = 0 ' Message Length High.
+        frame(5) = 6 ' Message Length Low(6 bytes to follow).
+        frame(6) = 1 ' The Unit Identifier.
+        frame(7) = 6 ' Function.
+        frame(8) = CByte(startAddress / 256) ' Starting Address High.
+        frame(9) = CByte(startAddress Mod 256) ' Starting Address Low.
+        frame(10) = byteArray(1) ' Write Data High.
+        frame(11) = byteArray(0) ' Write Data Low.
+        Return frame
+    End Function
+
+    Private Function Write(ByVal frame As Byte()) As Integer
+        Return Me.mSocket.Send(frame, frame.Length, SocketFlags.None)
+    End Function
+
+    Private Sub btn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+        Try
+            Dim slaveAddress As Byte = 1 'The Unit Identifier.
+            Dim functionCode As Byte = 6 ' Function.
+            Dim id As UShort = functionCode ' Transaction Identifier.
+            Dim startAddress As UShort = txt_add.Text ' Starting Address.
+            Dim data As UShort = UShort.Parse(TextBox1.Text) ' Write Data.
+            Dim frame As Byte() = Me.WriteSingleCoil(id, slaveAddress, startAddress, functionCode, data)
+            Me.Write(frame) ' Write frame to device.
+            Label8.Text = "succeed."
+            Thread.Sleep(100)
+        Catch ex As Exception
+            Label8.Text = "no succeed."
+            MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            Me.Connect()
+            Label4.Text = "connect."
+        Catch ex As Exception
+            Label4.Text = "no connect."
+            MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
+
+
